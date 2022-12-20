6052bce2066f80ede274b8f6af1b614ca446e231
Sean <seanzzz@Seans-Mac-Studio.local>
2022-12-20 16:58:37 -0700

    Update function in js and connect with html

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const quoteContainer = document.getElementById("quote-generator");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
