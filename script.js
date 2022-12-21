909b27eec6af205193a16eb4b1de48e4e7ccaa80
Sean <seanzzz@Seans-Mac-Studio.local>
2022-12-21 10:53:32 -0700

    Add loader

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const quoteContainer = document.getElementById("quote-generator");
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if Author field is blank and replace it with 'Unknown'
@@ -22,17 +36,20 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // Set Quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
-  const response = await fetch(apiUrl);
-  apiQuotes = await response.json();
-  newQuote();
+
   try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
   } catch (error) {
     alert(error);
     // console.log(error)
